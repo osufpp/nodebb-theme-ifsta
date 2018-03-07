cf33f266d53f39a086aa91e3aa1e2ab7cdfaa047
Chase Meadors <c.ed.mead@gmail.com>
2018-03-06 19:29:33 -0600

    Use a hardcoded Persona version that's not tied to OUR version.
    Apparently, instead of forking Persona theme, we have a post-install
    hook that DOWNLOADS the Persona theme and copies all of its files except
    the ones we want to override. We also import Persona theme in the
    theme.less file. Unfortunately, the version of Persona installed was
    chosen to be the same as THIS package's version, which creates...
    trouble, if any changes are made to this package. Use a hardcoded
    version that represents the version of Persona we're basing the theme
    off of.

--- install.js
+++ install.js
@@ -3,10 +3,13 @@ var fs = require('fs');
 var npm = require('npm');
 var path = require('path');
 
-var version = JSON.parse(fs.readFileSync('./package.json')).version;
+// The version of Persona Theme that this theme is based on / modifies
+var personaVersion = '2.1.12';
 
+// Install that version of Persona theme, and copy all of its templates into our templates directory
+// The templates already existing (i.e., the ones we modified), however, will not be overwritten (-n)
 npm.load(function () {
-    return npm.commands.install('dedupe', ['nodebb-theme-persona@' + version], function () {
+    return npm.commands.install('dedupe', ['nodebb-theme-persona@' + personaVersion], function () {
         var cmd = 'cp -r -n ./dedupe/node_modules/nodebb-theme-persona/templates/* ./templates';
         console.log(cmd)
         exec(cmd);
